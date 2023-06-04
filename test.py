81372991fa60ba398649d8dc0b57b124133aa9a5
Ameya Keskar <mycodestreak@gmail.com>
2023-06-04 19:48:59 +0530

    Working on Day/Night loop

--- test.py
+++ test.py
@@ -1,10 +1,43 @@
 from Character.human import Human
+from Shop.daShop import Shop
+import time, asyncio
+
+
+class GameVars:
+  def __init__(self):
+    self.dnCyc = 2 #2min Day Night Cycle
+    self.day = True #start with a day
+    self.dayCount = 0
+    self.gameStartTime = time.time() 
+    self.gameWon = False #to load stats accordingly
+
+
+
+
 
 if __name__ == '__main__':
   print('GAME STARTS...\n\n')
   _username = input('Type in a Username: ')
   player = Human(_username)
+  gmv = GameVars()
+  shop = Shop('7/11')
   print(f'\n\nCharacter {player.name} created. Some Game Variables declared...')
+  print(f'Hey, {player.name}! Welcome to the Game. The following Instructons might come in handy, please go through them and hit ENTER once done.\n')
+  print(f'1. The Goal of the game is \'Survival\'......')
+  rw = input() #the game will start after the user hits ENTER
+
+  #game loop
+  
+    
+
+    
+
+
+  
+  
+    
+ 
+
 
 
 
